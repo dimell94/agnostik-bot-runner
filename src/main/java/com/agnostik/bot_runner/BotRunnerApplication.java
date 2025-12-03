9b02189d3eb3c1a44b1d636d1fb88eddc6fbd33a
dimell94 <dim.ell.094@gmail.com>
2025-12-03 12:47:59 +0200

    feat: add application.yml , configuration and enable config in main

--- src/main/java/com/agnostik/bot_runner/BotRunnerApplication.java
+++ src/main/java/com/agnostik/bot_runner/BotRunnerApplication.java
@@ -1,9 +1,14 @@
 package com.agnostik.bot_runner;
 
+import com.agnostik.bot_runner.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableConfigurationProperties(AppProperties.class)
 public class BotRunnerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/agnostik/bot_runner/config/AppProperties.java
@@ -0,0 +1,27 @@
+package com.agnostik.bot_runner.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Data
+@Configuration
+@EnableConfigurationProperties(AppProperties.class)
+@ConfigurationProperties(prefix = "app")
+public class AppProperties {
+
+    private String baseUrl;
+    private String wsEndpoint;
+    private List<BotCredential> bots;
+
+
+    @Data
+    public static class BotCredential {
+
+        private String username;
+        private String password;
+    }
+}
