d66cb9e85954c0c1c47545b71b2c99dbf96cb0e3
dimell94 <dim.ell.094@gmail.com>
2025-12-03 12:18:22 +0200

    intial commit

---
+++ src/main/java/com/agnostik/bot_runner/BotRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.agnostik.bot_runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotRunnerApplication.class, args);
+	}
+
+}
