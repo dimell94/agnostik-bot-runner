a71c58d012b87753783e0969336a44861c687b0a
dimell94 <dim.ell.094@gmail.com>
2025-12-04 14:05:35 +0200

    fix: fixes in build gradle and main

--- src/main/java/com/agnostik/bot_runner/BotRunnerApplication.java
+++ src/main/java/com/agnostik/bot_runner/BotRunnerApplication.java
@@ -1,18 +1,29 @@
 package com.agnostik.bot_runner;
 
 import com.agnostik.bot_runner.config.AppProperties;
+import com.agnostik.bot_runner.config.LlmProperties;
+import com.agnostik.bot_runner.bot.BotManager;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableConfigurationProperties(AppProperties.class)
+@EnableConfigurationProperties({AppProperties.class, LlmProperties.class})
 public class BotRunnerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BotRunnerApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(BotManager manager) {
+		return args -> {
+
+			System.out.println("Bot runner started");
+		};
+	}
 }
